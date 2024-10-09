4c4567649898ab865971489a4727379ffc0a4f4e
Ewwweee <gabriel.bocquet@imt-atlantique.net>
2024-10-09 10:48:19 +0200

    showtimes + booking clinet

--- booking/booking.py
+++ booking/booking.py
@@ -1,22 +1,48 @@
 import grpc
 from concurrent import futures
-import booking_pb2
-import booking_pb2_grpc
+import showtime_pb2
+import showtime_pb2_grpc
+# import booking_pb2
+# import booking_pb2_grpc
 import json
 
-class BookingServicer(booking_pb2_grpc.BookingServicer):
+# class BookingServicer(booking_pb2_grpc.BookingServicer):
 
-    def __init__(self):
-        with open('{}/data/bookings.json'.format("."), "r") as jsf:
-            self.db = json.load(jsf)["schedule"]
+#     def __init__(self):
+#         with open('{}/data/bookings.json'.format("."), "r") as jsf:
+#             self.db = json.load(jsf)["schedule"]
 
-def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    booking_pb2_grpc.add_BookingServicer_to_server(BookingServicer(), server)
-    server.add_insecure_port('[::]:3002')
-    server.start()
-    server.wait_for_termination()
+# def serve():
+#     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+#     booking_pb2_grpc.add_BookingServicer_to_server(BookingServicer(), server)
+#     server.add_insecure_port('[::]:3002')
+#     server.start()
+#     server.wait_for_termination()
 
+def get_schedule_by_date(stub,date):
+    schedule = stub.GetMoviesByDate(date)
+    print("schedule : ", schedule.movies)
+
+def get_all_schedule(stub):
+    empty = showtime_pb2.Empty()
+    schedules = stub.GetSchedule(empty)
+    print("schedules : ", schedules.schedules[0].date)
+
+def run():
+    print("Run")
+    with grpc.insecure_channel('localhost:3002') as channel:
+        print("Channel loaded")
+        stub = showtime_pb2_grpc.ShowtimeStub(channel)
+
+        print("-------------- GetDateSchedule --------------")
+        date = showtime_pb2.Date(date="20151130")
+        print("date loaded : ", date.date)
+        get_schedule_by_date(stub, date)
+        print("-------------- GetAllSchedule --------------")
+        get_all_schedule(stub)
+
+    channel.close()
 
 if __name__ == '__main__':
-    serve()
+    # serve()
+    run()
