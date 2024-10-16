ac377936b0b5eb4c287b921735bd9c6b25dbbb27
Ewwweee <gabriel.bocquet@imt-atlantique.net>
2024-10-16 10:25:56 +0200

    Bookings finished

--- booking/booking.py
+++ booking/booking.py
@@ -15,11 +15,20 @@ class BookingServicer(booking_pb2_grpc.BookingServicer):
     
     def GetAllBookings(self, request, context):
         """This method returns all the bookings made by all the users """
-        print("self.bd = ", self.db)
-        return booking_pb2.AllBookings(bookings=self.db) # Marche une fois sur deux ?
+        print("self.bd = ", {"bookings" :self.db}, "\n")
+        res = booking_pb2.AllBookings(bookings=[]) #{"bookings" :self.db})
+        for booking in self.db:
+            bookingsUser = booking_pb2.BookingsUser(userid=booking["userid"],dates=[])
+            for date in booking["dates"]:
+                dateItem = booking_pb2.DateItem(date=date["date"],movies=date["movies"])
+                bookingsUser.dates.append(dateItem)
+            res.bookings.append(bookingsUser)
+        print("res : ", MessageToDict(res), "\n\n")
+        return res 
         
 
     def GetBookingForUser(self, request, context):
+        """Return the bookings made by the user request.id"""
         for booking in self.db : 
             if booking["userid"] == request.id :
                 print("Booking Found !")
@@ -27,12 +36,66 @@ class BookingServicer(booking_pb2_grpc.BookingServicer):
         return booking_pb2.BookingsUser(userid = "", dates = "" )
 
     def GetMovieAtDate(self,request,context):
+        """Return the movies available at the date request.date"""
         with grpc.insecure_channel('localhost:3002') as channel:
             stub = showtime_pb2_grpc.ShowtimeStub(channel)
             date = showtime_pb2.Date(date=request.date)
             schedule = get_schedule_by_date(stub, date)
             print("schedule = ", MessageToDict(schedule))
             return booking_pb2.ScheduleB(date=schedule.date, movies=schedule.movies)
+
+    def AddBookingByUser(self,request,context):
+        """Add a booking for the user request.user_id if it is possible"""
+        # Firstly we check if the movie is available at the date request.date
+        # We get the movies wich will be projected at the date request.date
+        with grpc.insecure_channel('localhost:3002') as channel:
+            stub = showtime_pb2_grpc.ShowtimeStub(channel)
+            date = showtime_pb2.Date(date=request.new_movie.date)
+            schedule = get_schedule_by_date(stub, date)
+        # If the movie is projected at request.date
+        if schedule != [] and request.new_movie.movieid in schedule.movies:
+            # We can add a booking
+            for user in self.db:
+                if user["userid"] == request.user_id.id:
+                    # We found the good user
+                    # We check if the user didn't already book this movie at the date request.date
+                    for date in user["dates"]:
+                        if date["date"] == request.new_movie.date:
+                            if not request.new_movie.movieid in date["movies"]:
+                                # We can finally add the booking
+                                date["movies"].append(request.new_movie.movieid)
+                                # We write in the database
+                                print("Booking added")
+                                write(self.db)
+                                # We return an answer
+                                return booking_pb2.BookingsUser(userid=user["userid"],dates=user["dates"])
+                    # The user didn't do any booking at the date given
+                    user["dates"].append({"date":request.new_movie.date,"movies":[request.new_movie.movieid]})
+                    # We write in the database
+                    print("Booking added with a new date")
+                    write(self.db)
+                    # We return an answer
+                    return booking_pb2.BookingsUser(userid=user["userid"],dates=user["dates"])
+            # The user didn't do any reservation, we need to at him/her/they into our database
+            new_user = {
+                "userid": request.user_id.id,
+                "dates": [{
+                    "date":request.new_movie.date,
+                    "movies":[request.new_movie.movieid]
+                }]}
+            self.db.append(new_user)
+            # We write in the database
+            print("Booking added with a new user")
+            write(self.db)
+            # We return an answer
+            return booking_pb2.BookingsUser(userid=new_user["userid"],dates=new_user["dates"])
+        return booking_pb2.BookingsUser(userid="",dates=[{"date":"","movies": [""]}])
+
+def write(movies):
+    """This function replace the database with the new one"""
+    with open('{}/data/bookings.json'.format("."), 'w') as f:
+        
+        json.dump({"bookings":movies}, f)
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
@@ -53,13 +116,14 @@ def get_all_schedule(stub):
     print("schedules : ", schedules.schedules[0].movies)
 
 # def run():
+#     """Code made to test the showtime service"""
 #     print("Run")
 #     with grpc.insecure_channel('localhost:3002') as channel:
 #         print("Channel loaded")
 #         stub = showtime_pb2_grpc.ShowtimeStub(channel)
 
 #         print("-------------- GetDateSchedule --------------")
-#         date = showtime_pb2.Date(date="20151130")
+#         date = showtime_pb2.Date(date="20151131")
 #         print("date loaded : ", date.date)
 #         print(get_schedule_by_date(stub, date).movies)
 #         print("-------------- GetAllSchedule --------------")
